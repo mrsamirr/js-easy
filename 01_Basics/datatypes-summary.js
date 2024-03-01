6d0542a7d69ee11c73fa0e49df388ddf30694b00
Md Samer Ansari <mdsameransari45@gmail.com>
2024-03-01 23:00:27 +0530

    datatypes summary updated

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 3345346596326946546n
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["ironman","thor","dr strange","hulk","spiderman"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+// omdwenf
