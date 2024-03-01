ce3e8e20d866f1d7d18022a5131a909602656da6
Md Samer Ansari <mdsameransari45@gmail.com>
2024-03-01 08:38:53 +0530

    dataTypes Updated

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+// code readability should be high
+
+console.log(3 + 3);
+
+console.log("Samir")
+
+
+let name = "samirr"
+let age = 19
+
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
+
