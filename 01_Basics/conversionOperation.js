762672a2f8ed60d9bf861147e4e77f08e2ab1597
Md Samer Ansari <mdsameransari45@gmail.com>
2024-03-01 09:02:35 +0530

    conversion of DataTypes

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,2 +1,32 @@
-let score = 33
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score) // conversion from string to number 
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"  => 33
+// "33abc" => NaN
+// null => 0
+// true => 1; false => 0
+
+
+let isLoggedIn = "samir"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "samir" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
 
