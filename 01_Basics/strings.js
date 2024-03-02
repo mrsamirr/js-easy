1955aa5473f4b9b3d5d26fee104e57a0af49464e
Samer <mdsameransari45.com>
2024-03-02 15:37:17 +0530

    Numbers and Maths

--- 01_Basics/strings.js
+++ 01_Basics/strings.js
@@ -3,12 +3,12 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value" );
 
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc')
 
 // console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// console.log(gameName.__proto__); // output => {}
 
 
 // console.log(gameName.length);
@@ -17,19 +17,21 @@ console.log(gameName.charAt(4)); // finding character index value
 console.log(gameName.indexOf('t')); // finding the character index 
 
 const newString = gameName.substring(0, 4)
-console.log(newString);
+console.log(newString);  // hite
 
 const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+console.log(anotherString); // ite
 
 const newStringOne = "  samir   "
 // console.log(newStringOne);
-// console.log(newStringOne.trim())
+// console.log(newStringOne.trim()) // trim out all spaces
 
 const url = "https://samir.com/samir%20ansari"
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-')) // replace value in a string
 
 console.log(url.includes('samir')) // return boolean true/false
 
 console.log(gameName.split('-'))
+
+
