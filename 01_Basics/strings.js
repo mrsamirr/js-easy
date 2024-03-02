b6db9ff3bebc908c302ac95abdf08db5180114b4
Md Samer Ansari <mdsameransari45@gmail.com>
2024-03-02 11:46:18 +0530

    strings js

---
+++ 01_Basics/strings.js
@@ -0,0 +1,35 @@
+const name = "samirr"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value" );
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4)); // finding character index value
+console.log(gameName.indexOf('t')); // finding the character index 
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "  samir   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim())
+
+const url = "https://samir.com/samir%20ansari"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('samir')) // return boolean true/false
+
+console.log(gameName.split('-'))
